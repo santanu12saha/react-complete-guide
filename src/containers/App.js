9288de60722ca3810938f715ed4aa04545954d7d
santanu12saha <saha.santanu0217@gmail.com>
2019-12-25 16:35:56 +0530

    Adding stateful props in app.js (this.props.title)

--- src/containers/App.js
+++ src/containers/App.js
@@ -62,7 +62,8 @@ class App extends Component {
    
     return (
         <div className={classes.App}>
-            <Cockpit 
+            <Cockpit
+              appTitle={this.props.title} 
               showPersons={this.state.showPersons}
               persons={this.state.persons}
               clicked={this.togglePersonsHandler}/>
