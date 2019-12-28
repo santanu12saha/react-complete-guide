3ed9664e92e9ad7620e01e682776f69e7919aa51
santanu12saha <saha.santanu0217@gmail.com>
2019-12-28 21:48:01 +0530

    using ref focus on input text

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -18,6 +18,9 @@ class Person extends Component {
     
     componentDidMount(){
         console.log("[Person.js] Inside componentDidMount()");
+        if( this.props.position === 0 ){
+            this.inputElement.focus();
+        }
     }
     
     render(){
@@ -26,7 +29,11 @@ class Person extends Component {
         return (
             <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input
+                    ref={(inp) => { this.inputElement = inp }} 
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}/>
             </Aux>
         )
 
