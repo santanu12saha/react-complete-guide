7f59f9f489e0167cdb56c0f3c91b43080ab6ad25
santanu12saha <saha.santanu0217@gmail.com>
2019-12-28 14:02:01 +0530

    StateFull-Higher-Component for withClass.js

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const withClass = (WrappedComponent, className) => {
-
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent {...props}/>
-        </div>
-    )
-}
+    return class extends Component {
+        render(){
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props}/>
+                </div>
+            )
+        }
+    }
+} 
 
 export default withClass;
