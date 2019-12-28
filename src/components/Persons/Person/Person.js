fdb95eef4909d4cf1bed7f1ea861f2f03de0bba3
santanu12saha <saha.santanu0217@gmail.com>
2019-12-28 22:26:09 +0530

    Using Ref_api React.createRef()

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -10,6 +10,7 @@ class Person extends Component {
     constructor(props){
         super(props);
         console.log("[Person.js] Inside Constructor", props);
+        this.inputElement = React.createRef();
     }
     
     UNSAFE_componentWillMount(){
@@ -19,7 +20,7 @@ class Person extends Component {
     componentDidMount(){
         console.log("[Person.js] Inside componentDidMount()");
         if( this.props.position === 0 ){
-            this.inputElement.focus();
+            this.inputElement.current.focus();
         }
     }
     
@@ -30,7 +31,7 @@ class Person extends Component {
             <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
                 <input
-                    ref={(inp) => { this.inputElement = inp }} 
+                    ref={this.inputElement} 
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name}/>
